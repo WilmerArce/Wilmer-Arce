906726322a97c2ccdb811074f9ae811ad213e6d3
WilmerArce <wilmer.arce@est.iudigital.edu.co>
2025-03-02 15:26:02 +0000

    actividad 1

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Wilmer Arce Chajin",
+    author_email="wilmer.arce@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+)
